1afd1cb20886614ebc4626243d2f6c424a97ca04
Projektili <99502851+Projektili@users.noreply.github.com>
2022-02-12 19:55:00 +0100

    Add support for clicking interface :)

---
+++ draw_squares.py
@@ -0,0 +1,5 @@
+from actions import draw_square
+
+if __name__ == "__main__":
+    draw_square()
+    input("Done")
---
+++ iki_specification.py
@@ -0,0 +1,6 @@
+from actions import iki_specification
+
+if __name__ == "__main__":
+    iki_specification()
+    input("Done")
+    
